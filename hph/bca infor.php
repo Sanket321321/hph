d6ce84c68c0978f6895fa9da6e04ceb882aa722b
sanket tanaji kharate <118279821+Sanket321321@users.noreply.github.com>
2023-05-12 07:14:17 +0530

    Add files via upload

---
+++ hph/bca infor.php
@@ -0,0 +1,40 @@
+Write PHP script to generate an XML code in the following format
+<?xml version=1.0”?>
+<ABC College>
+<Computer Application Department>
+<Course> BCA(Science) </Course>
+<Student Strength > 80</Student Strength>
+<Number of Teachers>12</Number of Teachers>
+</ABC College>
+</Computer Application Department>
+-->
+
+<?xml version="1.0" encoding="UTF-8" ?>
+
+<ABC College>
+<Computer Application Department>
+<Course> BCA(Science) </Course>
+<Student Strength > 80</Student Strength>
+<Number of Teachers>12</Number of Teachers>
+</Computer Application Department>
+
+<Computer Application Department>
+<Course> BCA(Science) </Course>
+<Student Strength > 80</Student Strength>
+<Number of Teachers>12</Number of Teachers>
+</Computer Application Department>
+
+
+<Computer Application Department>
+<Course> BCA(Science) </Course>
+<Student Strength > 80</Student Strength>
+<Number of Teachers>12</Number of Teachers>
+</Computer Application Department>
+
+  <Computer Application Department>
+<Course> BCA(Science) </Course>
+<Student Strength > 80</Student Strength>
+<Number of Teachers>12</Number of Teachers>
+</Computer Application Department>
+  
+</ABC College>
---
+++ hph/cricket.php
@@ -0,0 +1,37 @@
+Write a PHP script to following xml file.
+<cricket>
+<player> abe</player>
+<rums>100</runs>
+<wickets>20</wickets>
+</cricket>
+Store data for 5 players and display data of players who have scored more than 100 runs
+-->
+<?xml version="1.0" encoding="UTF-8" ?>
+<cricket>
+ <player> abe</player>
+ <rums>100</runs>
+ <wickets>20</wickets> 
+</cricket> 
+<cricket>
+ <player> abe</player>
+ <rums>100</runs>
+ <wickets>20</wickets>
+</cricket>
+
+ <cricket>
+ <player> abe</player>
+ <rums>100</runs>
+ <wickets>20</wickets>
+</cricket>
+
+  <cricket>
+ <player> abe</player>
+ <rums>100</runs>
+ <wickets>20</wickets>
+</cricket>
+
+ <cricket>
+ <player> abe</player>
+ <rums>100</runs>
+ <wickets>20</wickets>
+</cricket>
